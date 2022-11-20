2823b9cf8badc1b040f80099698aee4760fa88da
vapordev <61780088+vaporlite@users.noreply.github.com>
2022-11-21 00:43:46 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,81 +1,17 @@
-getgenv().Version = "- Script Discovery"
+if game.PlaceId == 11346342371 then
 
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vaporlite/vaporlite-hub/main/main.lua"))()
 
-local Window = Rayfield:CreateWindow({
-    Name = "Vapor Artworks ".. Version,
-    LoadingTitle = "Loading Hub...",
-    LoadingSubtitle = "by majestical",
-    ConfigurationSaving = {
-        Enabled = false,
-        FolderName = "vaporartworks",
-        FileName = "vaporlitefile"
-    },
-        Discord = {
-            Enabled = true,
-            Invite = "34tJXDPzvq",
-            RememberJoins = false
-        },
-    KeySystem = true,
-    KeySettings = {
-        Title = "Vapor Artworks",
-        Subtitle = "Key System",
-        Note = "Join the discord (discord.gg/34tJXDPzvq)",
-        FileName = "vaporartworks_key",
-        SaveKey = false,
-        GrabKeyFromSite = true,
-        Key = "https://pastebin.com/raw/ZZnFNvdT" --tostring(KeyLol)
-    }
-})
+elseif game.PlaceId == 10598587051 then
 
-local Discovery = Window:CreateTab("Script Discovery", 7072716155) -- Title, Image
-local DiscoverySection = Discovery:CreateSection("Normal GUIs")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vaporlite/anime-clicker-fight-source/main/ok.lua"))()
 
-local makingmemesinurbasement = Discovery:CreateButton({
-    Name = "WORKING | making memes in your basement at 3am",
-    Callback = function()
+elseif game.PlaceId == 11330149751 then
 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/vaporlite/vaporlite-hub/main/main.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vaporlite/scam-calls-addon/main/main.lua"))()
 
-    end,
-})
+elseif game.PlaceId == 10674429038 then
 
-local animefightclick = Discovery:CreateButton({
-    Name = "WORKING | Anime Click Fight",
-    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vaporlite/pickaxe-sim-infmoney/main/source_code.lua"))()
 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/vaporlite/anime-clicker-fight-source/main/ok.lua"))()
-
-    end,
-})
-
-local DiscoverySection2 = Discovery:CreateSection("Inf Cash GUIs")
-
-local scamgame = Discovery:CreateButton({
-    Name = "PATCHED | making scam calls to save your best friend",
-    Callback = function()
-
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/vaporlite/scam-calls-addon/main/main.lua"))()
-
-    end,
-})
-
-local pickaxesim = Discovery:CreateButton({
-    Name = "WORKING | Pickaxe Simulator - Infinite Money & co.",
-    Callback = function()
-
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/vaporlite/pickaxe-sim-infmoney/main/source_code.lua"))()
-
-    end,
-})
-
-
-
-
-
-local Credits = Window:CreateTab("Credits", 7072716155) -- Title, Image
-local CreditsSection = Credits:CreateSection("Developers & Contributors")
-
-Credits:CreateLabel("Script Developer : rekt#7724 aka majestical")
-Credits:CreateLabel("Inf Money Methods : rekt#7724 aka majestical")
-Credits:CreateLabel("UI : Rayfield UI")
+end
